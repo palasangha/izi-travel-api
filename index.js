9b2bbf0fda12ce6fa271f83153f099ace8ba79b2
JustinPMitchell <j.mitchell.real@gmail.com>
2018-02-20 20:56:24 -0800

    added to header

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ izi.prototype.cityData = function (city, key) {
   var url = 'https://cors-anywhere.herokuapp.com/https://api.izi.travel/mtg/objects/search?languages=en,ru&includes=translations&type=city&query=' + city;
   return fetch(url, {
     headers: {
-      "X-IZI-API-KEY": key
+      "X-IZI-API-KEY": key,
+      "origin", "http://localhost"
     }
   })
   .then(response => response.json()) // parses response to JSON
@@ -23,7 +24,8 @@ izi.prototype.museumData = function (cityUuid, key) {
   var url = 'https://cors-anywhere.herokuapp.com/https://api.izi.travel/cities/' + cityUuid + '/children?languages=en&type=museum';
   return fetch(url, {
     headers: {
-      "X-IZI-API-KEY": key
+      "X-IZI-API-KEY": key,
+      "origin", "http://localhost"
     }
   })
   .then(response => response.json()) // parses response to JSON
